ab304b80474dc0eef1c2264b661a3ae4ee48f574
rijaramalanjaona <rijamamitiana.ramalanjaona@ingesup.com>
2019-06-06 15:03:00 +0200

    backEnd : initialisation de la structure du projet avec SpringInitializr

---
+++ src/main/java/com/rija/dev/GestionContactsApplication.java
@@ -0,0 +1,13 @@
+package com.rija.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionContactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionContactsApplication.class, args);
+    }
+
+}
