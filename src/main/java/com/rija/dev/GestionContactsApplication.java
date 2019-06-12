e2841aa5ed929697271f168ee10854a8b9927fa0
rijaramalanjaona <rijamamitiana.ramalanjaona@ingesup.com>
2019-06-12 19:12:30 +0200

    ajout de CrossOrigin dans le rest controller

--- src/main/java/com/rija/dev/GestionContactsApplication.java
+++ src/main/java/com/rija/dev/GestionContactsApplication.java
@@ -21,13 +21,13 @@ public class GestionContactsApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        /*DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
         contactRepository.save(new Contact("nom1", "prenom1", dateFormat.parse("01/10/1990"),
                 "toto1@test.com", 11111, "nom1.png"));
         contactRepository.save(new Contact("nom2", "prenom2", dateFormat.parse("02/10/1990"),
                 "toto2@test.com", 11111, "nom2.png"));
         contactRepository.save(new Contact("nom3", "prenom3", dateFormat.parse("03/10/1990"),
-                "toto3@test.com", 11111, "nom3.png"));
+                "toto3@test.com", 11111, "nom3.png"));*/
 
         contactRepository.findAll().forEach(c-> {
             System.out.println(c.getNom());
